2e206034ec0a9caf6316c0dff5255fc765d781a8
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2023-09-10 10:49:31 +0200

    redesigning

--- src/App.js
+++ src/App.js
@@ -8,37 +8,51 @@ export default function App() {
   const [displayLocation, setDisplayLocation] = useState("");
   const [weather, setWeather] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(
     function () {
+      // const controller = new AbortController();
+
       async function fetchWeather() {
         try {
+          setError("");
           if (location.length < 2) return;
           setIsLoading(true);
           // 1) Getting location (geocoding)
           const geoRes = await fetch(
             `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
+            // ,{ signal: controller.signal }
           );
           const geoData = await geoRes.json();
 
           if (!geoData.results) throw new Error("Location no found");
           const { latitude, longitude, timezone, name, country_code } =
             geoData.results.at(0);
+          console.log(`${name} ${convertToFlag(country_code)}`);
           setDisplayLocation(`${name} ${convertToFlag(country_code)}`);
 
           // 2) Getting actual weather
           const weatherRes = await fetch(
             `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+            //, { signal: controller.signal }
           );
           const weatherData = await weatherRes.json();
-          setWeather(weatherData);
+          console.log(weatherData);
+          setWeather(weatherData.daily);
         } catch (err) {
+          console.error(err.message);
+          setError(err.message);
         } finally {
           setIsLoading(false);
         }
       }
       fetchWeather();
       localStorage.setItem("location", location);
+
+      // return () => {
+      //   controller.abort();
+      // };
     },
     [location]
   );
@@ -54,13 +68,14 @@ export default function App() {
         />
       </div>
       {isLoading && <p className="loader">Loading...</p>}
-      {weather.weathercode && (
+      {!isLoading && !error && weather.weathercode && (
         <Weather weather={weather} location={displayLocation} />
       )}
+      {error && <p className="error">{error}!!!</p>}
     </div>
   );
 }
-function Weather(location, weather) {
+function Weather({ location, weather }) {
   const {
     temperature_2m_max: max,
     temperature_2m_min: min,
@@ -69,7 +84,7 @@ function Weather(location, weather) {
   } = weather;
   return (
     <div>
-      <h2>Weather</h2>
+      <h2>Weather {location}</h2>
       <ul className="weather">
         {dates.map((date, i) => (
           <Day
@@ -86,7 +101,7 @@ function Weather(location, weather) {
   );
 }
 
-function Day(date, max, min, code, isToday) {
+function Day({ date, max, min, code, isToday }) {
   return (
     <li className="day">
       <span>{getWeatherIcon(code)}</span>
